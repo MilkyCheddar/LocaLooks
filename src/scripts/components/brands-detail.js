def3741ee967d2caf13ffc5f5cfe1862b3459ffd
bimamib <bimaprast1601@gmail.com>
2023-06-12 21:28:32 +0700

    add script brands-detail.js (progress)

--- src/scripts/components/brands-detail.js
+++ src/scripts/components/brands-detail.js
@@ -5,7 +5,31 @@ class BrandsDetail extends HTMLElement {
   
     render() {
       this.innerHTML =  `
-      /* masukin html disini */
+      <section>
+        <div class="container">
+          <article class="post-item">
+            <img src="images/ErigoT-ShirtOversizeAntelopeBlack.webp" class="picture" alt="Erigo">
+              <div class="post-item__content">
+                <h1 class="post-item__products">Products : Erigo</h1>
+                <h2 class="post-item__name">Name : Erigo T-Shirt Oversize Antelope Black</h2>
+                <h3 class="post-item__type">Type : Shirt</h3>
+                <p class="post-item__description">Description : The HD Oversize Erigo T-Shirt is currently the best choice for achieving a stylish look with high-quality products and a wide range of color options.</p>
+              </div>
+          </article>
+        </div>
+        
+        <div class="container">
+          <article class="post-item">
+            <img src="images/ErigoT-ShirtOversizeAntelopeBlack.webp" class="picture" alt="Erigo">
+              <div class="post-item__content">
+                <h1 class="post-item__products">Products : Erigo</h1>
+                <h2 class="post-item__name">Name : Erigo T-Shirt Oversize Antelope Black</h2>
+                <h3 class="post-item__type">Type : Shirt</h3>
+                <p class="post-item__description">Description : The HD Oversize Erigo T-Shirt is currently the best choice for achieving a stylish look with high-quality products and a wide range of color options.</p>
+              </div>
+          </article>
+        </div>
+      </section>
       `;
     }
   }
