d6da8dfc3a03381fd561182bff7d3412e4ee9b5b
bimamib <bimaprast1601@gmail.com>
2023-06-14 23:38:01 +0700

    adding script detail brands compass

--- src/scripts/components/brands-detail.js
+++ src/scripts/components/brands-detail.js
@@ -41,6 +41,42 @@ class BrandsDetail extends HTMLElement {
               </div>
           </article>
         </div>
+
+        <div class="container">
+          <article class="post-item">
+            <img src="https://cms.sepatucompass.com/images/productdetail/82fee0dd9cb3f8a4fdb775287069dc9873b5fa42.png" class="picture" alt="Compass">
+              <div class="post-item__content">
+                <h1 class="post-item__products">Products : Compass</h1>
+                <h2 class="post-item__name">Name : Compass Gazelle Low Black White</h2>
+                <h3 class="post-item__type">Type : Shoes</h3>
+                <p class="post-item__description">Description : The original silhouette of Compass that started it all in 1998. Re-designed to combine classic vintage with modern details, the Gazelle Low is your ideal every day footwear.</p>
+              </div>
+          </article>
+        </div>
+        
+        <div class="container">
+          <article class="post-item">
+            <img src="https://cms.sepatucompass.com/images/productdetail/4a94777eee55eacffb593e2b011c0d5460f40eef.png" class="picture" alt="Compass">
+              <div class="post-item__content">
+                <h1 class="post-item__products">Products : Compass</h1>
+                <h2 class="post-item__name">Name : Compass Flight Jkt Low</h2>
+                <h3 class="post-item__type">Type : Shoes</h3>
+                <p class="post-item__description">Description : Compass® Retrograde Flight JKT edition is inspired by the jacket worn by air force pilots. Unused army green materials was sourced and repurposed to create an authentic and fresh take on an iconic military look.</p>
+              </div>
+          </article>
+        </div>
+
+        <div class="container">
+          <article class="post-item">
+            <img src="https://cms.sepatucompass.com/images/productdetail/6ebd14afe7e78277b8cba83cc1c4c1c1f212e118.png" class="picture" alt="Compass">
+              <div class="post-item__content">
+                <h1 class="post-item__products">Products : Compass</h1>
+                <h2 class="post-item__name">Name : Compass Gazelle Low Black Gum</h2>
+                <h3 class="post-item__type">Type : Shoes</h3>
+                <p class="post-item__description">Description : The original silhouette of Compass that started it all in 1998. Re-designed to combine classic vintage with modern details, the Gazelle Low is your ideal every day footwear.</p>
+              </div>
+          </article>
+        </div>
       </section>
       `;
     }
