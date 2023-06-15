ffb36ff63d627e614e33baa389713992590d3688
MilkyCheddar <fransiskus.widianto@gmail.com>
2023-06-15 23:48:28 +0700

    adding icon, refixing home pages, fixing data.json

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,15 +1,25 @@
+import homeBrand from '../templates/home-creator';
+
 const Home = {
-    async render() {
-      return /* html */`
-        <home-page></home-page>
-      `;
-    },
-  
-    async afterRender() {
-      const navbar = document.querySelector('navigation-bar');
-      navbar.classList.remove('hidden');
-    },
-  };
-  
-  export default Home;
-  
+  async render() {
+    return /* html */ `
+      <brandPick></brandPick>
+    `;
+  },
+
+  async afterRender() {
+    let picks = ['Erigo', 'Compass', 'Hijack Sandals'];
+
+    const navbar = document.querySelector('navigation-bar');
+    navbar.classList.remove('hidden');
+    const brandPicks = document.querySelector('#content');
+    
+    for (let i = 0; i < picks.length; i++) {
+      const brandPickElement = picks[i];
+      const brandPick = homeBrand(brandPickElement);
+      brandPicks.innerHTML += brandPick;
+    }    
+  },
+};
+
+export default Home;
