7fcb777d852f42ffb0750d2f4cd51d9b9caee882
bimamib <bimaprast1601@gmail.com>
2023-06-17 04:33:51 +0700

    perubahan script pada pages detail

--- src/scripts/components/brands-detail.js
+++ src/scripts/components/brands-detail.js
@@ -7,24 +7,24 @@ class BrandsDetail extends HTMLElement {
     render() {
       this.innerHTML =  `
       <section>
-        <h2 class="brand_name">${brands.name}</h2>
-        <img class="brand_image"> src="${API_ENDPOINT.BRAND_IMAGE + brand.thumbnail}" alt="${brand.name}" />
-        <div class="brand_info">
+        <h2 class="brands_name">${this._productName}</h2>
+        <img class="brands_image"> src="${this._product.image_url}" alt="brands_name" />
+        <div class="brands_info">
             <h3>Information</h3>
-            <div class = categories>
+            <div class = category>
                 <h4>Kategori</h4>
-                <p>${brand.categories}</p>
+                <p>${this._product.category}</p>
             </div>
             <div id="catalogs">
-                <div class="brand">
-                    <h4>Katalog</h4>
-                    <p>${brand.brands.catalogs.map((brand) => `- ${brand.name}`).join('<br>')}</p>
-                    <h5>Type</h5>
-                    <p>${brand.brands.type.catalogs.map((brand) => `- ${brand.type.name}`).join('<br>')}</p>
-                </div>
+                // <div class="brand">
+                //     <h4>Katalog</h4>
+                //     <p>${brand.brands.catalogs.map((brand) => `- ${brand.name}`).join('<br>')}</p>
+                //     <h5>Type</h5>
+                //     <p>${brand.brands.type.catalogs.map((brand) => `- ${brand.type.name}`).join('<br>')}</p>
+                // </div>
             </div>
             <h4>Description</h4>
-            <p>${brand.description}</p>
+            <p>${this._product.description}</p>
         </div>
       </section>
       `;
